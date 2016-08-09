d7ec7ee58b834a74b77856a5a3f03c4fd98ddd4f
wabarr <wabarr@gmail.com>
2016-08-09 16:56:37 -0400

    refactor choices into separate files for clarity

---
+++ census_paleo/choices_taxonomy.py
@@ -0,0 +1,77 @@
+# #choices for upcoming taxonomy class
+CHOICES_KINGDOM = (
+("Animalia", "Animalia"),
+)
+
+CHOICES_PHYLUM = (
+("Chordata", "Chordata"),
+)
+
+CHOICES_CLASS = (
+("Mammalia", "Mammalia"),
+("Reptilia", "Reptilia"),
+("Pisces", "Pisces"),
+("Aves", "Aves"),
+("Amphibia", "Amphibia"),
+)
+
+CHOICES_ORDER = (
+("ARTIODACTYLA", "ARTIODACTYLA"),
+("CARNIVORA", "CARNIVORA"),
+("PERISSODACTYLA", "PERISSODACTYLA"),
+("PRIMATES", "PRIMATES"),
+("PROBOSCIDEA", "PROBOSCIDEA"),
+("RODENTIA", "RODENTIA"),
+("AFROSORICIDA", "AFROSORICIDA"),
+("CETACEA", "CETACEA"),
+("CHIROPTERA", "CHIROPTERA"),
+("CINGULATA", "CINGULATA"),
+("DASYUROMORPHIA", "DASYUROMORPHIA"),
+("DERMOPTERA", "DERMOPTERA"),
+("DIDELPHIMORPHIA", "DIDELPHIMORPHIA"),
+("DIPROTODONTIA", "DIPROTODONTIA"),
+("ERINACEOMORPHA", "ERINACEOMORPHA"),
+("HYRACOIDEA", "HYRACOIDEA"),
+("LAGOMORPHA", "LAGOMORPHA"),
+("MACROSCELIDEA", "MACROSCELIDEA"),
+("MICROBIOTHERIA", "MICROBIOTHERIA"),
+("MONOTREMATA", "MONOTREMATA"),
+("NOTORYCTEMORPHIA", "NOTORYCTEMORPHIA"),
+("PAUCITUBERCULATA", "PAUCITUBERCULATA"),
+("PERAMELEMORPHIA", "PERAMELEMORPHIA"),
+("PHOLIDOTA", "PHOLIDOTA"),
+("PILOSA", "PILOSA"),
+("SCANDENTIA", "SCANDENTIA"),
+("SIRENIA", "SIRENIA"),
+("SORICOMORPHA", "SORICOMORPHA"),
+("TUBULIDENTATA", "TUBULIDENTATA"),
+
+)
+
+CHOICES_RANK = (
+('CLASS', 'CLASS'),
+('ORDER', 'ORDER'),
+('FAMILY', 'FAMILY'),
+('SUBFAMILY', 'SUBFAMILY'),
+('TRIBE', 'TRIBE'),
+('GENUS', 'GENUS'),
+('SPECIES', 'SPECIES'),
+('SUBSPECIES', 'SUBSPECIES'),
+('INFRAORDER', 'INFRAORDER'),
+('SUBGENUS', 'SUBGENUS'),
+('SUBORDER', 'SUBORDER'),
+('SUPERFAMILY', 'SUPERFAMILY'),
+)
+
+CHOICES_tribe = (
+("Tragelaphini", "Tragelaphini"),
+("Cephalophini", "Cephalophini"),
+("Bovini", "Bovini"),
+("Hippotragini", "Hippotragini"),
+("Reduncini", "Reduncini"),
+("Alcelaphini", "Alcelaphini"),
+("Antilopini", "Antilopini"),
+("Aepycerotini", "Aepycerotini"),
+("Neotragini", "Neotragini"),
+)
+
